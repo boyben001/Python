c94dcf9054d7319136a7e641f6f8febaac2bed39
Ben Chen <boyben001@gmail.com>
2021-01-06 13:04:12 +0800

    1/6

---
+++ main.py
@@ -0,0 +1,27 @@
+from pytube import YouTube
+from bs4 import BeautifulSoup
+import re
+import requests
+import requests.packages.urllib3
+import json
+requests.packages.urllib3.disable_warnings()
+
+
+songElect = []
+languageElect = []
+typeElect = []
+artistElect = []
+
+
+# def showRandomSong():
+
+
+# def songRecord():
+
+
+'''
+html_data = requests.get(url, verify=False)
+soup = BeautifulSoup(html_data.content, 'html.parser')
+photo = soup.find_all('img')
+print(photo)
+'''
